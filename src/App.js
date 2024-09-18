12d437c6ed80aed0652c44a893fbda21075cdc2f
ozantalay <ozntalay@hotmail.com>
2024-09-18 18:42:54 +0300

    baslangic

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const ONE =
+  "https://images.pexels.com/photos/2249528/pexels-photo-2249528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+const TWO =
+  "https://images.pexels.com/photos/1061141/pexels-photo-1061141.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+const THREE =
+  "https://images.pexels.com/photos/2249530/pexels-photo-2249530.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+const FOUR =
+  "https://images.pexels.com/photos/1061139/pexels-photo-1061139.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+const FIVE =
+  "https://images.pexels.com/photos/1010973/pexels-photo-1010973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+const SIX =
+  "https://images.pexels.com/photos/4772874/pexels-photo-4772874.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+
+function App() {
+  return <Captcha />;
+}
+
+const Captcha = () => {
+  // KODUNUZ BURAYA GELECEK
+};
+
+export default App;
